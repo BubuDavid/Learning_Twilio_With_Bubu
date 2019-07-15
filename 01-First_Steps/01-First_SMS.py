42c4513fc56d50b347bb4ae29d1728cc328c81b4
Bubu <david.pedroza.segoviano@gmail.com>
2019-07-15 02:15:18 -0500

    Ups

--- 01-First_Steps/01-First_SMS.py
+++ 01-First_Steps/01-First_SMS.py
@@ -1,16 +1,19 @@
 from twilio.rest import Client
 
-# Your Account SID from twilio.com/console
-account_sid = "ACa74ae815adb448751e79554f2f64e4c8"
+# La SID de tu cuenta de Twilio, o "Identificador de Seguridad"
+# The SID of your Twilio account or "Security Identifier"
+account_sid = my_Account_SID
 
+# Tu "Ficha de autor" de Twilio
 # Your Auth Token from twilio.com/console
-auth_token = "5ea7e1589b6142a1c96f4ed075ebccc9"
+auth_token = my_account_token
 
+#To work with the APIs you need 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-	to = "+524774027085",
-	from_ = "+17194254101",
+	to = my_phone_number,
+	from_ = phone_number_bought,
 	body = "Hello, this is my message from python") 
 
 print(message.sid)
