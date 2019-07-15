61d7daa20a83d0d33fd141897244f80ae1c7e954
Bubu <david.pedroza.segoviano@gmail.com>
2019-07-15 12:57:57 -0500

    First Lecture

--- 01-First_Steps/01-First_SMS.py
+++ 01-First_Steps/01-First_SMS.py
@@ -1,12 +1,14 @@
 from twilio.rest import Client
+import os
 
 # La SID de tu cuenta de Twilio, o "Identificador de Seguridad"
 # The SID of your Twilio account or "Security Identifier"
-account_sid = my_Account_SID
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 
 # Tu "Ficha de autor" de Twilio
 # Your Auth Token from twilio.com/console
-auth_token = my_account_token
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+
 
 #To work with the APIs you need this two things
 client = Client(account_sid, auth_token)
